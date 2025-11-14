b5131aec8ffc02b8ee6bb1b3d30e5e523208b39f
Dmitri Laptev <sougift@gmail.com>
2025-11-14 22:31:00 +0100

    use evn variable for getting all categories in vite..config

--- storefront/vite.config.ts
+++ storefront/vite.config.ts
@@ -1,58 +1,70 @@
 import { fetchCategoriesFromPayload, fetchProductsFromPayload } from './src/lib/payload-ssg'
 import graphqlLoader from 'vite-plugin-graphql-loader'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig, loadEnv } from 'vite'
 import svgLoader from 'vite-svg-loader'
 import vue from '@vitejs/plugin-vue'
-import { defineConfig } from 'vite'
 import path from 'path'
 // https://vite.dev/config/
-export default defineConfig({
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: '@use "@/assets/styles/sass-utils/_variables.scss" as *;',
+
+export default defineConfig(({ mode }) => {
+  return {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "@/assets/styles/sass-utils/_variables.scss" as *;',
+        },
       },
     },
-  },
-  plugins: [vue(), vueDevTools(), svgLoader(), graphqlLoader()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@payload-types': path.resolve(__dirname, '../payload/src'),
+    plugins: [vue(), vueDevTools(), svgLoader(), graphqlLoader()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+        '@payload-types': path.resolve(__dirname, '../payload/src'),
+      },
     },
-  },
-  server: {
-    port: 8000,
+    server: {
+      port: 8000,
 
-    watch: {
-      ignored: ['!../payload/src/payload-types.ts'],
+      watch: {
+        ignored: ['!../payload/src/payload-types.ts'],
+      },
     },
-  },
-  ssgOptions: {
-    script: 'async',
-    //formatting: 'minify',
-    async includedRoutes(paths, routes) {
-      const locales = (process.env.VITE_LANGUAGES || 'de,en').split(',')
-      const dynamicRoutes: string[] = []
+    ssgOptions: {
+      script: 'async',
+      //formatting: 'minify',
+      async includedRoutes(paths, routes) {
+        const env = loadEnv(mode, process.cwd(), '')
 
-      // 1. Categories
-      const categories = await fetchCategoriesFromPayload()
-      for (const cat of categories) {
-        for (const locale of locales) {
-          dynamicRoutes.push(`/${locale}/category/${cat.slug}`)
+        const locales = (env.VITE_LANGUAGES || 'de').split(',')
+        const dynamicRoutes: string[] = []
+
+        // 1. Categories
+
+        const responseCategories = await fetch(`${env.VITE_BACKEND_URL}/categories?limit=0&depth=1`)
+        const categoriesData = await responseCategories.json()
+        const categories = categoriesData.docs || []
+
+        for (const cat of categories) {
+          for (const locale of locales) {
+            dynamicRoutes.push(`/${locale}/category/${cat.slug}`)
+          }
         }
-      }
 
-      // // 2. Items
-      // const products = await fetchProductsFromPayload()
-      // for (const product of products) {
-      //   for (const locale of locales) {
-      //     dynamicRoutes.push(`/${locale}/item/${product.slug}`)
-      //   }
-      // }
+        // const responseItems = await fetch(`${env.VITE_BACKEND_URL}/products?limit=0&depth=2`)
+        // const itemsData = await responseItems.json()
+        // const products = itemsData.docs || []
+        // // 2. Items
+        // const products = await fetchProductsFromPayload()
+        // for (const product of products) {
+        //   for (const locale of locales) {
+        //     dynamicRoutes.push(`/${locale}/item/${product.slug}`)
+        //   }
+        // }
 
-      // console.log('PRODUCTS/ CATEGORIES', products, categories)
-      return [...locales.map((loc) => `/${loc}`), ...dynamicRoutes]
+        // console.log('PRODUCTS/ CATEGORIES', products, categories)
+        return [...locales.map((loc) => `/${loc}`), ...dynamicRoutes]
+      },
     },
-  },
+  }
 })
