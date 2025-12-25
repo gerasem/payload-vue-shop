9b5ed6dce18a84c01c468ffcda716bc09fbeb583
Dmitri Laptev <sougift@gmail.com>
2025-12-26 00:53:51 +0100

    add icons and create dummy pages

--- store/app/pages/index.vue
+++ store/app/pages/index.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
-    <h1>{{ $t('welcome') }}</h1>
-    
-      <BaseButton>
-        Nuxt UI Button
-      </BaseButton>
+    <h1>Home</h1>
   </div>
 </template>
 
 <script setup lang="ts">
 definePageMeta({
   layout: 'default'
+})
+
+useHead({
+  title: 'Home | Store - Quality Products at Great Prices',
+  meta: [
+    { name: 'description', content: 'Welcome to Store. Discover our wide selection of quality products at competitive prices. Shop electronics, clothing, home goods, and more.' }
+  ]
 })
 </script>
