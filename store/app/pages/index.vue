64d36cd12e9347451919bdc0f740df6407080fff
Dmitri Laptev <sougift@gmail.com>
2025-12-25 23:40:10 +0100

    nuxt: add i18n

---
+++ store/app/pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>{{ $t('welcome') }}</h1>
+    <div>
+      <template v-for="(locale, index) in locales" :key="locale.code">
+        <NuxtLink :to="switchLocalePath(locale.code)">
+          {{ locale.name }} ({{ locale.code }})
+        </NuxtLink>
+        <span v-if="index < locales.length - 1"> / </span>
+        <!-- Debug output -->
+        <pre style="font-size: 10px; display: inline;">[{{ switchLocalePath(locale.code) }}]</pre>
+      </template>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { useSwitchLocalePath, useI18n } from '#imports'
+
+const switchLocalePath = useSwitchLocalePath()
+const { locales } = useI18n()
+</script>
--- store/nuxt.config.ts
+++ store/nuxt.config.ts
@@ -1,5 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  imports: {
+    autoImport: false
+  },
+  modules: ['@nuxtjs/i18n'],
+  i18n: {
+    locales: [
+      { code: 'en', file: 'en.json', name: 'En' },
+      { code: 'de', file: 'de.json', name: 'De' }
+    ],
+    defaultLocale: 'de',
+    strategy: 'prefix_except_default',
+    langDir: 'locales',
+  },
+  devServer: {
+    port: 3001
+  }
 })
