9341c8520bd6f330f86a7c83b552b32754473ed3
gerasem <sougift@gmail.com>
2026-02-19 23:47:41 +0100

    fix path style for supabase storage

--- payload/src/payload.config.ts
+++ payload/src/payload.config.ts
@@ -101,6 +101,7 @@ export default buildConfig({
             config: {
               endpoint: process.env.S3_ENDPOINT,
               region: process.env.S3_REGION,
+              forcePathStyle: true, // Required for Supabase Storage
               credentials: {
                 accessKeyId: process.env.S3_ACCESS_KEY_ID || '',
                 secretAccessKey: process.env.S3_SECRET_ACCESS_KEY || '',
