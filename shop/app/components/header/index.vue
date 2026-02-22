cc556e600f4ef871004d94bea8b951e957fb54fb
gerasem <sougift@gmail.com>
2026-02-22 22:27:22 +0100

    upload new logo

--- shop/app/components/header/index.vue
+++ shop/app/components/header/index.vue
@@ -52,7 +52,7 @@ const logoSvg = computed(() => headerData.value?.icon?.svgContent || '')
         class="flex items-center gap-3 lg:ml-0 lg:mr-5 absolute left-1/2 -translate-x-1/2 lg:static lg:translate-x-0"
       >
         <!-- Render logo from Payload SVG -->
-        <div v-if="logoSvg" v-html="logoSvg" class="h-12 lg:-mt-1 flex items-center" />
+        <div v-if="logoSvg" v-html="logoSvg" class="header__logo h-12 lg:-mt-1 flex items-center" />
       </NuxtLink>
 
       <!-- Desktop only content -->
@@ -140,3 +140,12 @@ const logoSvg = computed(() => headerData.value?.icon?.svgContent || '')
     </template>
   </UHeader>
 </template>
+
+<style lang="scss">
+.header__logo {
+ & svg {
+  height: 100%;
+  width: 100%;  
+ }
+}
+</style>
